7b0170cda490aaf0c244aa6f79c72bb02cf3f854
Artemchik542 <borodinov@ngs.ru>
2020-11-09 14:20:10 +0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/bank_deposit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bank_deposit.dir/main.c.obj"
+  "bank_deposit.exe"
+  "bank_deposit.exe.manifest"
+  "bank_deposit.lib"
+  "bank_deposit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bank_deposit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

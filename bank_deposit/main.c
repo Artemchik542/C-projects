8eda2da7812c06d494f750eea83b4604dc140a7e
Artemchik542 <borodinov@ngs.ru>
2020-12-05 21:20:15 +0700

    little fix

--- bank_deposit/main.c
+++ bank_deposit/main.c
@@ -6,11 +6,11 @@ int main() {
     scanf("%lf%d%lf", &hard_sum, &mon, &per);
     //printf("%f", hard_sum);
     easy_sum = hard_sum;
-    easy_sum += easy_sum * per * mon;
+    easy_sum += easy_sum * per/1200 * mon;
     printf("%f\n", easy_sum);
     for (int i = 1; i <= mon; i++){
         hard_sum += hard_sum * per/1200;
-        printf("%d %f  %f\n", i, hard_sum);
+        printf("%d %f\n", i, hard_sum);
     }
     return 0;
 }
